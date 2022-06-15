f019001b7455f4d4dca4f91c16dd0a8ba082e5df
Rocio <rociomdlc@gmail.com>
2022-06-15 21:17:32 +0200

    esto es una modificacion

--- src/holaMundo/holaMundo.java
+++ src/holaMundo/holaMundo.java
@@ -3,7 +3,7 @@ package holaMundo;
 public class holaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo, soy Blanca. Y no sé si hacer este código en java");
+		System.out.println("Hola mundo, soy Blanca. Y no sé si hacer este código en java. Pero hay que intentarlo");
 
 	}
 
