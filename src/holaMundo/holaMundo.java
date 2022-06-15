998fb95a16fa1829d7e4c0ae154fee026f1f4952
FutureTechIsFemale <futuretechisfemale@gmail.com>
2022-06-15 20:50:52 +0200

    Primer commit, Chispas

---
+++ src/holaMundo/holaMundo.java
@@ -0,0 +1,10 @@
+package holaMundo;
+
+public class holaMundo {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo, soy Blanca. Y no sé si hacer este código en java");
+
+	}
+
+}
